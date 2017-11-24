21b13af84aecbac0d9342a57915ea0b88b933be5
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-11-24 08:11:01 +0700

    6.day11-upload

--- routers/posts.js
+++ routers/posts.js
@@ -1,31 +1,63 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
+var multer  = require('multer');
 
 const Post = require('../schema/Post');
 const User = require('../schema/User');
 
+
+//multer config
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+const upload = multer({storage: storage});
+
 router.get('/posts', function(req, res){
-  Post.findAll({
-    include: [
-      {model: User}
-    ],
-    order: [
-      ["id", 'DESC']
-    ]
-  }).then(function(posts){
-    res.send(posts)
-  })
+  const include = [
+    {model: User}
+  ];
+
+  const order = [
+    ["id", 'DESC']
+  ];
+
+  if(req.query && req.query.userId){
+    const userId = req.query.userId;
+
+    Post.findAll({
+      include: include,
+      order: order,
+      where: {
+        userId: userId
+      }
+    }).then(function(posts){
+      res.send(posts)
+    })
+  }else{
+    Post.findAll({
+      include: include,
+      order: order
+    }).then(function(posts){
+      res.send(posts)
+    })
+  }
+
 });
 
 router.post('/posts', upload.single('photo'), function(req, res){
-  console.log(req.file);
+  const fullUrl = req.protocol + '://' + req.get('host');
+  let body = req.body;
+  body.imageUrl = fullUrl + "/" + req.file.path;
 
-  // Post.create(req.body).then(function(post){
-  //   res.send(post);
-  // })
+  Post.create(body).then(function(post){
+    res.send(post);
+  })
 });
 
 module.exports = router;
